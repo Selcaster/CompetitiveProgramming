c4398c4acbc2e43b8e129fd95a79236b02e79914
Selcaster <selcankk-ap215@code.edu.az>
2024-10-14 16:48:11 +0400

    duzeltmek isteyende daha da pis gune qoydum

--- Kodlasdirma/Kodlasdirma.cpp
+++ Kodlasdirma/Kodlasdirma.cpp
@@ -49,19 +49,36 @@
 //	for (int i = 0;i < n;i++)cout << "*";
 //}
 //*Cercive 4
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int n, m;
+//	cin >> n >> m;
+//	for (int i = 0;i < m;i++)cout << "*";
+//	for (int i = 0;i < n - 2;i++)
+//	{
+//		cout << endl << "*";
+//		for (int j = 0;j < m - 2;j++)cout << " ";
+//		if (m > 1) cout << "*";
+//	}
+//	cout << endl;
+//	if (n > 1)for (int i = 0;i < m;i++)cout << "*";
+//}
+//Sablon 3
 #include <iostream>
 using namespace std;
 int main()
 {
-	int n, m;
-	cin >> n >> m;
-	for (int i = 0;i < m;i++)cout << "*";
-	for (int i = 0;i < n - 2;i++)
+	int n;
+	cin >> n;
+	for (int i = 0; i < n/2+1;i++)
 	{
-		cout << endl << "*";
-		for (int j = 0;j < m - 2;j++)cout << " ";
-		if (m > 1) cout << "*";
+		for (int j = 0;j < i;j++)cout << " ";
+		cout << '*';
+		for (int j = 0;j < n - 2 * (i + 1);j++)cout << " ";
+		if (n % 2 == 0)cout << '*';
+		for (int j = 0;j < i;j++)cout << " ";
+		cout << endl;
 	}
-	cout << endl;
-	if (n > 1)for (int i = 0;i < m;i++)cout << "*";
 }
